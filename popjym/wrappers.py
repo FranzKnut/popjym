d7f3f39ee03b8914df95c2bf1f6efc0eb1d879c0
julian <julian.lemmel@tuwien.ac.at>
2024-07-15 12:08:01 +0900

    remove faulty import

--- popjym/wrappers.py
+++ popjym/wrappers.py
@@ -1,6 +1,7 @@
 import jax
 import jax.numpy as jnp
 from flax import struct
+from functools import partial
 from gymnax.wrappers.purerl import (
     GymnaxWrapper,
     Optional,
@@ -8,7 +9,6 @@ from gymnax.wrappers.purerl import (
     Union,
     chex,
     environment,
-    partial,
     spaces,
 )
 
